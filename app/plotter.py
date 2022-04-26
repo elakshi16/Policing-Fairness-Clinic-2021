bebd05dd1ff7f2e2f289ac612a58b0ea322a7884
SaminZachariah <saminzachariah@gmail.com>
2022-04-25 22:45:42 -0700

    Rewrote plotting to ensure consistency. Added labeling for published material

--- app/plotter.py
+++ app/plotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
+import os
 
 def basic_plot(node_dict, save_file=False):
     """
@@ -26,4 +27,98 @@ def basic_plot(node_dict, save_file=False):
     if save_file:
         plt.savefig(save_file)
         plt.close()
+
+def final_plot(node_dict, prec=False, spath=False,fname=False, title=False, mean=False, stddev=False, similarity=False):
+    x = []
+    y = []
+    scalval = []
+
+    for key, value in node_dict.items():
+        x.append(key[0])
+        y.append(key[1])
+        scalval.append(value)
+
+
+    fig, ax = plt.subplots()
     
+    if prec is True:
+        pc = ax.scatter(x,y, c=scalval, cmap='viridis', marker='s',edgecolor='none')
+    else:
+        pc = ax.scatter(x,y,s=1.75, c=scalval, cmap='viridis', marker='s',edgecolor='none')
+        
+    fig.colorbar(pc, ax=ax)
+
+    if title:
+        ax.set_title(title)
+    
+    output_str = ''
+    
+    if mean:
+        output_str += r'$\mu = $' + "{:.4f}".format(mean)
+    if stddev:
+        output_str += '\n' +  r'$\sigma=$' + "{:.4f}".format(stddev)
+    if similarity:
+        output_str += '\n similarity = ' + "{:.4f}".format(similarity)
+    
+    ax.text(min(x), min(y), output_str)
+
+
+    fig.set_size_inches(10,10)
+
+    if spath:
+        if not os.path.exists(spath):
+            os.makedirs(spath)
+        
+        plt.savefig(spath + fname, dpi=500)
+        plt.close()
+
+
+def imshow_plot(node_dict, spath=False,fname=False, title=False, mean=False, stddev=False, similarity=False):
+    xi = []
+    yi = []
+    scalval = []
+    for key, value in node_dict.items():
+        xi.append(key[0])
+        yi.append(key[1])
+        scalval.append(value)
+
+    wd = max(xi) - min(xi) + 1
+    ht = max(yi) - min(yi) + 1
+
+    arr = np.empty([wd, ht])
+    arr[:] = np.nan
+
+    for key, value in node_dict.items():
+        arr[key[0]-min(xi),key[1]-min(yi)] = value
+
+    fig, ax = plt.subplots()
+    
+    arr = np.rot90(arr)
+    pc = ax.imshow(arr, cmap='viridis')
+    
+    fig.colorbar(pc, ax=ax)
+    plt.xticks([])
+    plt.yticks([])
+
+    if title:
+        ax.set_title(title)
+    
+    output_str = ''
+    
+    if mean:
+        output_str += r'$\mu = $' + "{:.4f}".format(mean)
+    if stddev:
+        output_str += '\n' +  r'$\sigma=$' + "{:.4f}".format(stddev)
+    if similarity:
+        output_str += '\n similarity = ' + "{:.4f}".format(similarity)
+    
+    ax.set_xlabel(output_str)
+
+    fig.set_size_inches(10,10)
+
+    if spath:
+        if not os.path.exists(spath):
+            os.makedirs(spath)
+        
+        plt.savefig(spath + fname, dpi=500)
+        plt.close()
